6a18e0698d7c7057463f524d3dda08f18901d94f
crazycricketer003 <ajayganesh333@gmail.com>
2023-07-26 00:31:33 +0530

    day8/45

---
+++ Math/Reverseinteger.java
@@ -0,0 +1,15 @@
+package Math;
+
+public class Reverseinteger {
+    long sum=0;
+    while(x!=0)
+    {
+        sum=(sum*10)+x%10;
+        x=x/10;
+    }
+    
+    if(sum<Integer.MIN_VALUE || sum>Integer.MAX_VALUE)
+        return 0;
+    else
+        return (int)sum;
+}
